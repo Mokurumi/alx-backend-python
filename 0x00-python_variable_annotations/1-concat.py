3252ff1d0d0f20c7321950bdbde5c63e37efed70
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-02 14:54:57 +0300

    1-5

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Type-annotated function concat that takes a string str1
+and a string str2 as arguments
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Returns the concatenation of two strings
+    '''
+    return str1 + str2
