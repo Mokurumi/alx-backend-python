d0bddee2ed373a432292c392b88e3def085f857b
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-03 00:18:20 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,7 +38,7 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client._public_repos_url, mock_org["repos_url"])
 
     @patch("client.GithubOrgClient._public_repos_url",
-        new_callable=TEST_PAYLOAD)
+            new_callable=TEST_PAYLOAD)
     @patch("client.get_json")
     def test_public_repos(self, mock_get, mock_url):
         """
@@ -61,6 +61,8 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.has_license(repo, license_key), expected)
 
 
+@parameterized_class(['org_payload', 'repos_payload',
+                      'expected_repos', 'apache2_repos'], TEST_PAYLOAD)
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """
     TestIntegrationGithubOrgClient class
